cec58666e10dd1187ccd2791c11e8d6fead13611
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-08-02 13:36:54 +0530

    [Pankaj] Added query to get details of customer and orders

--- CustomerOrders.sql
+++ CustomerOrders.sql
@@ -28,5 +28,13 @@ VALUES (101, 1, '2023-07-21', 500.00);
 INSERT INTO orders (order_id, customer_id, order_date, total_amount)
 VALUES (102, 2, '2023-07-21', 350.50);
 
+SELECT c.customer_id, c.customer_name, c.email, c.phone,
+       o.order_id, o.order_date, o.total_amount
+FROM customers c
+LEFT JOIN orders o ON c.customer_id = o.customer_id;
 
+SELECT c.customer_id, c.customer_name, c.email, c.phone,
+       o.order_id, o.order_date, o.total_amount
+FROM customers c
+INNER JOIN orders o ON c.customer_id = o.customer_id;
 
